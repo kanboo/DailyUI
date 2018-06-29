6fd9d9b75df74acce02e5ea2e114a98e0c4e87fc
kanboo <wang.kanboo@gmail.com>
2018-06-29 11:15:35 +0800

    updata

---
+++ main.js
@@ -0,0 +1,196 @@
+var datas = [
+  {
+    no: 1,
+    title: '01. Sign Up',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_01.png',
+    codepen: 'https://codepen.io/Kanboo/pen/pVPBQG'
+  },
+  {
+    no: 2,
+    title: '02. Checkout',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_02.png',
+    codepen: 'https://codepen.io/Kanboo/pen/pVwyQj'
+  },
+  {
+    no: 3,
+    title: '03. Landing Page',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_03.png',
+    codepen: 'https://codepen.io/Kanboo/pen/PeKjJr'
+  },
+  {
+    no: 4,
+    title: '04. Calculator',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_04.png',
+    codepen: ''
+  },
+  {
+    no: 5,
+    title: '05. App Icon',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_05.png',
+    codepen: 'https://codepen.io/Kanboo/pen/bMoqPr'
+  },
+  {
+    no: 6,
+    title: '06. User Profile',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_06.png',
+    codepen: 'https://codepen.io/Kanboo/pen/qYPQdr'
+  },
+  {
+    no: 7,
+    title: '07. Settings',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_07.png',
+    codepen: 'https://codepen.io/Kanboo/pen/PeOZqr'
+  },
+  {
+    no: 8,
+    title: '08. 404 Page',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_08.png',
+    codepen: 'https://codepen.io/Kanboo/pen/MGOGeJ'
+  },
+  {
+    no: 9,
+    title: '09. Music Player',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_09.png',
+    codepen: 'https://codepen.io/Kanboo/pen/JvMdzo'
+  },
+  {
+    no: 10,
+    title: '10. Social Share',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_10.gif',
+    codepen: 'https://codepen.io/Kanboo/pen/PeEPbR'
+  },
+  {
+    no: 11,
+    title: '11. Flash Messages',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_11.png',
+    codepen: 'https://codepen.io/Kanboo/pen/GdQWjX'
+  },
+  {
+    no: 12,
+    title: '12. Single Product',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_12.png',
+    codepen: 'https://codepen.io/Kanboo/pen/BxYWJe'
+  },
+  {
+    no: 13,
+    title: '13. Direct Messaging',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_13.png',
+    codepen: 'https://codepen.io/Kanboo/pen/KRQqQb'
+  },
+  {
+    no: 14,
+    title: '14. Countdown Timer',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_14.gif',
+    codepen: 'https://codepen.io/Kanboo/pen/JvZaRj'
+  },
+  {
+    no: 15,
+    title: '15. On/Off Switch',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_15.gif',
+    codepen: 'https://codepen.io/Kanboo/pen/MGXJYo'
+  },
+  {
+    no: 16,
+    title: '16. Pop-Up / Overlay',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_16.gif',
+    codepen: 'https://codepen.io/Kanboo/pen/xjzgbz'
+  },
+  {
+    no: 17,
+    title: '17. Email Receipt',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_17.gif',
+    codepen: 'https://codepen.io/Kanboo/pen/MGXJyv'
+  },
+  {
+    no: 19,
+    title: '19. Leaderboard',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_19.png',
+    codepen: 'https://codepen.io/Kanboo/pen/pVOOvw'
+  },
+  {
+    no: 23,
+    title: '23. Onboarding',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_23.gif',
+    codepen: 'https://codepen.io/Kanboo/pen/degBJd'
+  },
+  {
+    no: 26,
+    title: '#26. Subscribe',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_26.png',
+    codepen: 'https://codepen.io/Kanboo/pen/JvmQvr'
+  },
+  {
+    no: 37,
+    title: '37. Weather',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_37.png',
+    codepen: 'https://codepen.io/Kanboo/pen/OZBeqe'
+  },
+  {
+    no: 49,
+    title: '49. Notifications',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_49.gif',
+    codepen: 'https://codepen.io/Kanboo/pen/MGPNJv'
+  },
+  {
+    no: 53,
+    title: '53. Header Navigation',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_53.gif',
+    codepen: 'https://codepen.io/Kanboo/pen/bMJBad'
+  },
+  {
+    no: 54,
+    title: '54. Confirm Reservation',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_54.png',
+    codepen: 'https://codepen.io/Kanboo/pen/bMJBLd'
+  },
+  {
+    no: 56,
+    title: '56. Breadcrumbs',
+    imgUrl:
+      'https://raw.githubusercontent.com/kanboo/DailyUI/master/img/dailyui_56.jpg',
+    codepen: 'https://codepen.io/Kanboo/pen/vjMydP'
+  },
+  {
+    no: 61,
+    title: '61. Redeem Coupon',
+    imgUrl: '',
+    codepen: 'https://codepen.io/Kanboo/pen/zjXoaR'
+  },
+  {
+    no: 71,
+    title: '71. Schedule',
+    imgUrl: '',
+    codepen: 'https://codepen.io/Kanboo/pen/ZoZBwa'
+  }
+];
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    uiLists: datas
+  }
+});
